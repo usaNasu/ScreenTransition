14f39fc3ef678f298b246857668fe135b5cb7b86
tatsuyaouchi <s18b703@shohoku.ac.jp>
2019-10-10 13:58:51 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/HGUREMETAL/screentransition/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.shohoku.HGUREMETAL.screentransition;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
